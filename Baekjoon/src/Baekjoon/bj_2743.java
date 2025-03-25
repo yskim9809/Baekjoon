ef2de549382c0552a9281cf091557819527e177c
yskim9809 <dudtj9809@gmail.com>
2025-03-26 00:03:03 +0900

    20250326

---
+++ Baekjoon/src/Baekjoon/bj_2743.java
@@ -0,0 +1,15 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class bj_2743 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String word = scanner.nextLine();
+		String arr[] = word.split("");
+		System.out.println(arr.length);
+
+	}
+
+}
