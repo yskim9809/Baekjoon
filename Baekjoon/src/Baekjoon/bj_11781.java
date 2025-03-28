2c5fa9e40c1f2448308af1c8dc1ab00adfdac01b
yskim9809 <dudtj9809@gmail.com>
2025-03-28 18:01:24 +0900

    20250328

---
+++ Baekjoon/src/Baekjoon/bj_11781.java
@@ -0,0 +1,19 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class bj_11781 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		while (scanner.hasNextLine()) {
+			String str = scanner.nextLine();
+
+			System.out.println(str);
+		}
+		scanner.close();
+
+	}
+
+}
