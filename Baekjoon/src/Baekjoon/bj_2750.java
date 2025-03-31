f1cd935069875bc59d8dc238bae9561b6ba0fff5
yskim9809 <dudtj9809@gmail.com>
2025-04-01 02:17:47 +0900

    20250401

---
+++ Baekjoon/src/Baekjoon/bj_2750.java
@@ -0,0 +1,24 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class bj_2750 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
