d31f9f95d8bc31daf3563ea66f982de13e7536b7
yskim9809 <dudtj9809@gmail.com>
2025-04-10 00:05:48 +0900

    20250410

---
+++ Baekjoon/src/Baekjoon/bj_11005.java
@@ -0,0 +1,29 @@
+package Baekjoon;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class bj_11005 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextInt();
+		int b = sc.nextInt();
+		// 0~35 범위에 있는 두자리 숫자가 나올 수 있으니 StringBuilder보다는 stack 사용
+		Stack<Long> stack = new Stack<>();
+		while (n != 0) {// 진수 구하기
+			stack.push(n % b);
+			n /= b;
+		}
+		// 두자리수는 영어 알파벳으로 치환해서 출력
+		while (stack.size() != 0) {
+			long num = stack.pop();
+			if (num < 10)
+				System.out.print(num);
+			else
+				System.out.print((char) (num + 55));
+		}
+
+	}
+
+}
