e893fc6b77adffcce9c23f828432d2732204a2b6
yskim9809 <dudtj9809@gmail.com>
2025-04-17 22:43:12 +0900

    20250417

---
+++ Baekjoon/src/Baekjoon/bj_25305.java
@@ -0,0 +1,26 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class bj_25305 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int n = Integer.parseInt(st.nextToken()); // 응시자 수
+		int k = Integer.parseInt(st.nextToken()); // 상받는 사람의 수
+		int[] arr = new int[n];
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		System.out.println(arr[n - k]);
+
+	}
+
+}
