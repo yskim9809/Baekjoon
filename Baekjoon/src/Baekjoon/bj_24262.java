091481626e984416fb70f45605cf32dcf23778e9
yskim9809 <dudtj9809@gmail.com>
2025-05-12 23:27:15 +0900

    20250512

--- Baekjoon/src/Baekjoon/bj_24262.java
+++ Baekjoon/src/Baekjoon/bj_24262.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class bj_24262 {
 
 	public static void main(String[] args) {
-		// 어떤 수가 들어와도 값은 똑같음
+		// 어떤 수가 들어와도 1번만 수행됨 O(1) -> 최고차항 0
 		System.out.println(1);
 		System.out.println(0);
 
---
+++ Baekjoon/src/Baekjoon/bj_24263.java
@@ -0,0 +1,16 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class bj_24263 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		// 입력받은 값만큼 반복된다 O(n) -> 최고차항 1
+		System.out.println(a);
+		System.out.println(1);
+
+	}
+
+}
