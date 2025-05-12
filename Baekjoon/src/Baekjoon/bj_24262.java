307e9e585b3d5fc1e979a02f3b938d3fe191f05a
yskim9809 <dudtj9809@gmail.com>
2025-05-12 23:18:20 +0900

    20250512

---
+++ Baekjoon/src/Baekjoon/bj_24262.java
@@ -0,0 +1,14 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class bj_24262 {
+
+	public static void main(String[] args) {
+		// 어떤 수가 들어와도 값은 똑같음
+		System.out.println(1);
+		System.out.println(0);
+
+	}
+
+}
