2e363c449b3377e0716b0a51a3b22e3a5f95251f
yskim9809 <dudtj9809@gmail.com>
2025-05-14 23:52:32 +0900

    20250514

---
+++ Baekjoon/src/Baekjoon/bj_24265.java
@@ -0,0 +1,14 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class bj_24265 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		long a = scanner.nextLong(); // 처음부터 long
+		System.out.println(a * (a - 1) / 2);
+		System.out.println(2);
+	}
+
+}
