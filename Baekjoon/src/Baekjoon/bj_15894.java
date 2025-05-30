4c191be429acd0f25f5a665aad6b201776611166
yskim9809 <dudtj9809@gmail.com>
2025-05-31 00:12:17 +0900

    20250531

---
+++ Baekjoon/src/Baekjoon/bj_15894.java
@@ -0,0 +1,14 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class bj_15894 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		long i = scanner.nextLong();
+		System.out.println(i * 4);
+
+	}
+
+}
