14d5ddc4c553c17356782d54d9d26d2dfd1736e8
yskim9809 <dudtj9809@gmail.com>
2025-06-02 21:33:01 +0900

    20250602

--- Baekjoon/src/Baekjoon/bj_5073.java
+++ Baekjoon/src/Baekjoon/bj_5073.java
@@ -1,5 +1,6 @@
 package Baekjoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class bj_5073 {
@@ -7,6 +8,26 @@ public class bj_5073 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
+		while (true) {
+			int[] arr = new int[3];
+			for (int i = 0; i < 3; i++) {
+				arr[i] = scanner.nextInt();
+			}
+			if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0) {
+				break;
+			}
+			Arrays.sort(arr);
+			if (arr[0] == arr[1] && arr[1] == arr[2]) {
+				System.out.println("Equilateral");
+			} else if (arr[2] >= arr[0] + arr[1]) {
+				System.out.println("Invalid");
+			} else if (arr[0] != arr[1] && arr[0] != arr[2] && arr[1] != arr[2]) {
+				System.out.println("Scalene");
+			} else {
+				System.out.println("Isosceles");
+			}
+
+		}
 	}
 
 }
