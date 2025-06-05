c82dbabd620217b0d08b952adaed5e8d1305678b
yskim9809 <dudtj9809@gmail.com>
2025-06-06 00:03:10 +0900

    20250606

---
+++ Baekjoon/src/Baekjoon/bj_11050.java
@@ -0,0 +1,26 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class bj_11050 {
+	/*
+	 * 이항계수 : n개중에 k 개를 선택하는 경우의 수 n!/k!(n-k)!
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int k = scanner.nextInt();
+		System.out.println(factoreal(n) / (factoreal(k) * factoreal(n - k)));
+
+	}
+
+	public static int factoreal(int r) {
+		int result = 1;
+		for (int i = r; i > 1; i--) {
+			result *= i;
+		}
+		return result;
+
+	}
+
+}
