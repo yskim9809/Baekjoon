38feca186608ed30fe1b39be26430e9204a5a280
yskim9809 <dudtj9809@gmail.com>
2025-07-18 22:05:46 +0900

    20250718

--- Baekjoon/src/Baekjoon/bj_2606.java
+++ Baekjoon/src/Baekjoon/bj_2606.java
@@ -1,10 +1,41 @@
 package Baekjoon;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class bj_2606 {
+	static Scanner scanner = new Scanner(System.in);
+	static int n = scanner.nextInt();
+	static ArrayList<Integer>[] graph = new ArrayList[n + 1];
 
 	public static void main(String[] args) {
-		
+		for (int i = 1; i <= n; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		int m = scanner.nextInt();
+		scanner.nextLine();
+		for (int i = 0; i < m; i++) {
+			String[] st = scanner.nextLine().split(" ");
+			int a = Integer.parseInt(st[0]);
+			int b = Integer.parseInt(st[1]);
+			graph[a].add(b);
+			graph[b].add(a); // 반드시 양방향 추가
+		}
+		dfs(1);
+		System.out.println(count-1);
+	}
 
+	static boolean[] visited = new boolean[n + 1];
+	static int count = 0;
+
+	private static void dfs(int node) {
+		visited[node] = true;
+		count++;
+		for (int next : graph[node]) {
+			if (!visited[next]) {
+				dfs(next);
+			}
+		}
 	}
 
 }
