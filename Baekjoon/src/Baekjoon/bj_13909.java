6e26c181d30e9cb3b777c49e61f7090fcd6d1de5
yskim9809 <dudtj9809@gmail.com>
2025-08-23 23:46:38 +0900

    20250823

--- Baekjoon/src/Baekjoon/bj_13909.java
+++ Baekjoon/src/Baekjoon/bj_13909.java
@@ -1,9 +1,16 @@
 package Baekjoon;
 
+import java.util.Scanner;
+
 public class bj_13909 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		// 약수의 개수가 홀수인 경우 창문이 열림 ->완전제곱수 1 4 9 16 ...
+		long n = scanner.nextLong();
+		long ans = (long) Math.sqrt(n); // n개 이하의 제곱수 = 제곱근 정수부분
+
+		System.out.println(ans);
 
 	}
 
