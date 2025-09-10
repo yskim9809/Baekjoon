5265e32a56c81c395867e2482a1c1fa20273b451
yskim9809 <dudtj9809@gmail.com>
2025-09-10 17:30:21 +0900

    20250910

---
+++ Baekjoon/src/Baekjoon/bj_17144.java
@@ -0,0 +1,29 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj_17144 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int R = Integer.parseInt(st.nextToken()); // 행
+		int C = Integer.parseInt(st.nextToken()); // 열
+		int T = Integer.parseInt(st.nextToken()); // 시간
+
+		int[][] map = new int[R][C];
+
+		for (int i = 0; i < R; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < C; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+	}
+
+}
