bfef9649018dadb17274a143a2e20771057c4fae
yskim9809 <dudtj9809@gmail.com>
2025-10-23 23:47:28 +0900

    20251023

--- Baekjoon/src/Baekjoon/bj_1463.java
+++ Baekjoon/src/Baekjoon/bj_1463.java
@@ -9,21 +9,21 @@ public class bj_1463 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[] dp = new int[1000001]; // DP 배열 초기화
-		dp[2] = 1;
-		dp[3] = 1;
-		for (int i = 4; i <= n; i++) {
-			if (i % 6 == 0) {
-				dp[i] = Math.min(dp[i / 3], Math.min(dp[i / 2], dp[i - 1])) + 1;
-			} else if (i % 3 == 0) {
-				dp[i] = Math.min(dp[i / 3], dp[i - 1]) + 1;
-			} else if (i % 2 == 0) {
-				dp[i] = Math.min(dp[i / 2], dp[i - 1]) + 1;
-			} else {
-				dp[i] = dp[i - 1] + 1;
+		int dp[] = new int[n + 1];
+		// dp[i] = i를 1로 만드는 최소 연산 수
+		dp[1] = 0;
+		for (int i = 2; i <= n; i++) {
+			int best = dp[i - 1] + 1; // -1연산만 했을 때
+			if (i % 2 == 0) {
+				best = Math.min(best, dp[i / 2] + 1); // 지금 나누는것 + dp[i/2]에서 1로 가는 연산 수
 			}
+			if (i % 3 == 0) {
+				best = Math.min(best, dp[i / 3] + 1);
+			}
+			dp[i] = best;
+
 		}
-		System.out.print(dp[n]);
+		System.out.println(dp[n]);
 
 	}
 
