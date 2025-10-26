e88dd96287da02a03035c24e69f0c6c35449b75e
yskim9809 <dudtj9809@gmail.com>
2025-10-26 22:56:55 +0900

    20251026

--- Baekjoon/src/Baekjoon/bj_11726.java
+++ Baekjoon/src/Baekjoon/bj_11726.java
@@ -1,12 +1,14 @@
 package Baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class bj_11726 {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 		int dp[] = new int[n + 1];
 		// dp[n] = 2*n 타일을 덮는 경우의 수
 		// 맨 끝에 세로타일 dp[n-1], 가로타일두개 dp[n-2]를 놓는경우, 서로 겹치지 않는다
