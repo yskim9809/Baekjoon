64340985fec3f50345911b02a693bcd97b2ac1d8
yskim9809 <dudtj9809@gmail.com>
2025-10-26 22:57:10 +0900

    20251026

--- Baekjoon/src/Baekjoon/bj_11726.java
+++ Baekjoon/src/Baekjoon/bj_11726.java
@@ -8,8 +8,8 @@ public class bj_11726 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-		int dp[] = new int[n + 1];
+		int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[Math.max(3, n + 1)];
 		// dp[n] = 2*n 타일을 덮는 경우의 수
 		// 맨 끝에 세로타일 dp[n-1], 가로타일두개 dp[n-2]를 놓는경우, 서로 겹치지 않는다
 		dp[1] = 1;
