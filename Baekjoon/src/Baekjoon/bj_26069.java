0a6309e9e630de8fead0219c11b16c554e20f104
yskim9809 <dudtj9809@gmail.com>
2025-12-16 00:11:58 +0900

    20251216

---
+++ Baekjoon/src/Baekjoon/bj_26069.java
@@ -0,0 +1,27 @@
+package Baekjoon;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class bj_26069 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Set <String> set = new HashSet<>();
+		set.add("ChongChong");
+		int N = sc.nextInt();
+		while(N-- > 0) {
+			String A = sc.next();
+			String B = sc.next();
+			
+			if(set.contains(A) || set.contains(B)) {
+				set.add(A);
+				set.add(B);
+			}
+		}
+		System.out.println(set.size());
+
+	}
+
+}
