58e7bf6a31c3cd74e5e3edf649de04745d8c9770
yskim9809 <dudtj9809@gmail.com>
2025-12-22 16:43:06 +0900

    20251222

--- Baekjoon/src/Baekjoon/bj_1735.java
+++ Baekjoon/src/Baekjoon/bj_1735.java
@@ -1,10 +1,34 @@
 package Baekjoon;
 
+import java.util.Scanner;
+
 public class bj_1735 {
 
 	public static void main(String[] args) {
 		// 분수를 더하고 약분
+		Scanner scan = new Scanner(System.in);
+		int c1, c2, p1, p2;
 
+		// c1/p1, c2/p2
+		c1 = scan.nextInt();
+		p1 = scan.nextInt();
+		c2 = scan.nextInt();
+		p2 = scan.nextInt();
+
+		// 더해주고
+		c1 = c1 * p2 + c2 * p1;
+		p1 = p1 * p2;
+		int gcd = getGcd(c1, p1); // 최대공약수를 구해준다.
+
+		System.out.println(c1 / gcd + " " + p1 / gcd); // 기약분수 출력
+
+	}
+
+	public static int getGcd(int a, int b) {
+		if (a % b == 0) {
+			return b;
+		}
+		return getGcd(b, a % b);
 	}
 
 }
