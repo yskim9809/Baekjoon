9a3c9715da2615b2ba090a3774840532385c2902
yskim9809 <dudtj9809@gmail.com>
2025-12-23 23:07:46 +0900

    20251223

---
+++ Baekjoon/src/Baekjoon/bj_17103.java
@@ -0,0 +1,37 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj_17103 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+
+		// 소수 구하기 = 소수 false
+		// 에라스토테네스의 체
+		boolean[] num = new boolean[1000001];
+		num[0] = num[1] = true;
+		for (int i = 2; i * i <= 1000000; i++) {
+			if (!num[i]) {
+				for (int j = i + i; j <= 1000000; j += i) {
+					num[j] = true;
+				}
+			}
+		}
+
+		while (t-- > 0) {
+			int temp = Integer.parseInt(br.readLine());
+			int ans = 0;
+			for (int j = 2; j <= temp / 2; j++) {
+				if (!num[j] && !num[temp - j])
+					ans++;
+			}
+			System.out.println(ans);
+		}
+
+	}
+
+}
